f331f2bbb20537bb119a12e0ec53c660b6b7b7e9
fredslbn <fh.silaban@gmail.com>
2023-08-18 00:06:41 +0700

    jahsjdkel

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,39 +4,61 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SUPER.KERNEL-LAVENDER[CLO]
-do.devicecheck=0
+kernel.string=SUPER.KERNEL LAVENDER [CLO]
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=lavender
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
+is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
 
 ## AnyKernel boot install
-split_boot;
+dump_boot;
 
-flash_boot;
-flash_dtbo;
+## begin ramdisk changes
+# Set Android version for kernel
+ver="$(file_getprop /system/build.prop ro.build.version.release)"
+if [ ! -z "$ver" ]; then
+    patch_cmdline "androidboot.version" "androidboot.version=$ver"
+else
+    patch_cmdline "androidboot.version" ""
+fi
+
+write_boot;
 ## end boot install
+
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
 
