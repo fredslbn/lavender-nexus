69a1eda06a265d5a6d7660ce9fc4945b1f77ba50
fredslbn <fh.silaban@gmail.com>
2023-08-17 20:03:32 +0700

    hggfffghhjj

--- fs/exec.c
+++ fs/exec.c
@@ -1527,10 +1527,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-
-// KernelSU hook
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
+#ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags);
+#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1546,9 +1547,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-        ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
-	if (IS_ERR(filename))
-		return PTR_ERR(filename);
+    #ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+	   ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+	   ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	#endif
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
