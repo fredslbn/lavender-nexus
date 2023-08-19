2391bc68a240a6d398ba22c0cd3f4f397854ddf3
fredslbn <fh.silaban@gmail.com>
2023-08-19 23:24:07 +0700

    gahajsksk

--- .circleci/linaro.sh
+++ .circleci/linaro.sh
@@ -48,7 +48,7 @@ FINAL_ZIP_ALIAS=Karenulgarde-${TANGGAL}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=nexus14
+COMPILER=clang14-7
 
 ##----------------------------------------------------------##
 # Specify Linker
@@ -124,6 +124,29 @@ function cloneTC() {
     export KERNEL_CCOMPILE32="arm-linux-gnueabihf-"
     export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"
     
+    elif [ $COMPILER = "clang14-7" ];
+	then
+	#git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/ -b android10-gsi --depth 1 --no-tags --single-branch clang_all
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r450784e.tar.gz && mkdir clang && tar -xzvf clang-r450784e.tar.gz -C clang/
+    #mv clang_all/clang-r353983c clang
+    #rm -rf clang_all
+    export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
+    export KERNEL_CLANG="clang"
+    export PATH="$KERNEL_CLANG_PATH/bin:$PATH"
+    CLANG_VERSION=$(clang --version | grep version | sed "s|clang version ||")
+	
+    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
+    mv gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu gcc64
+    export KERNEL_CCOMPILE64_PATH="${KERNEL_DIR}/gcc64"
+    export KERNEL_CCOMPILE64="aarch64-linux-gnu-"
+    export PATH="$KERNEL_CCOMPILE64_PATH/bin:$PATH"
+    GCC_VERSION=$(aarch64-linux-gnu-gcc --version | grep "(GCC)" | sed 's|.*) ||')
+   
+    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
+    mv gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf gcc32
+    export KERNEL_CCOMPILE32_PATH="${KERNEL_DIR}/gcc32"
+    export KERNEL_CCOMPILE32="arm-linux-gnueabihf-"
+    export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"  
     
     elif [ $COMPILER = "clang17" ];
 	then
@@ -148,6 +171,32 @@ function cloneTC() {
     export KERNEL_CCOMPILE32_PATH="${KERNEL_DIR}/gcc32"
     export KERNEL_CCOMPILE32="arm-linux-gnueabihf-"
     export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"
+
+    elif [ $COMPILER = "clang17-7" ];
+	then
+	#git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/ -b android10-gsi --depth 1 --no-tags --single-branch clang_all	
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r498229.tar.gz && mkdir clang && tar -xzvf clang-r498229.tar.gz -C clang/
+    #mv clang_all/clang-r353983c clang
+    #rm -rf clang_all
+    export KERNEL_CLANG_PATH="${KERNEL_DIR}/clang"
+    export KERNEL_CLANG="clang"
+    export PATH="$KERNEL_CLANG_PATH/bin:$PATH"
+    CLANG_VERSION=$(clang --version | grep version | sed "s|clang version ||")
+	
+    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
+    mv gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu gcc64
+    export KERNEL_CCOMPILE64_PATH="${KERNEL_DIR}/gcc64"
+    export KERNEL_CCOMPILE64="aarch64-linux-gnu-"
+    export PATH="$KERNEL_CCOMPILE64_PATH/bin:$PATH"
+    GCC_VERSION=$(aarch64-linux-gnu-gcc --version | grep "(GCC)" | sed 's|.*) ||')
+   
+    wget https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz && tar -xf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
+    mv gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf gcc32
+    export KERNEL_CCOMPILE32_PATH="${KERNEL_DIR}/gcc32"
+    export KERNEL_CCOMPILE32="arm-linux-gnueabihf-"
+    export PATH="$KERNEL_CCOMPILE32_PATH/bin:$PATH"
+    
+
    
 	elif [ $COMPILER = "eva" ];
 	then
@@ -256,7 +305,7 @@ START=$(date +"%s")
 	# Compile
 	make O=out ARCH=arm64 ${DEFCONFIG}
 	
-	if [ -d ${KERNEL_DIR}/clang ];
+	if [ -d ${KERNEL_DIR}/clang1 ];
 	   then
 	       make -kj$(nproc --all) O=out \
 	       ARCH=arm64 \
@@ -274,6 +323,25 @@ START=$(date +"%s")
 	       READELF=llvm-readelf \
 	       OBJSIZE=llvm-size \
 	       V=$VERBOSE 2>&1 | tee error.log
+	
+	       
+	 elif [ -d ${KERNEL_DIR}/clang ];
+	   then
+	       make -kj$(nproc --all) O=out \
+	       ARCH=arm64 \
+	       CC=$KERNEL_CLANG \
+           CROSS_COMPILE=$KERNEL_CCOMPILE64 \
+           CROSS_COMPILE_ARM32=$KERNEL_CCOMPILE32 \
+           #CLANG_TRIPLE=aarch64-linux-gnu- \
+           LD=${LINKER} \
+           LLVM=1 \
+           LLVM_IAS=1 \
+           #AR=llvm-ar \
+           #NM=llvm-nm \
+           #OBJCOPY=llvm-objcopy \
+           #OBJDUMP=llvm-objdump \
+           #STRIP=llvm-strip \
+	       V=$VERBOSE 2>&1 | tee error.log      
 	       
 	elif [ -d ${KERNEL_DIR}/cosmic ];
 	   then
